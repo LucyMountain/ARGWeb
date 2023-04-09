3159f279ee32a16a7289f5c420d9c3d2e4540d93
LucyMountain <lucymountain1@icloud.com>
2023-04-09 12:58:54 +0100

    randomising text

--- countdown/views.py
+++ countdown/views.py
@@ -6,15 +6,4 @@ import datetime
 
 
 def index(request):
-    first_time = datetime.datetime.now()
-    later_time = datetime.datetime(2023, 4, 10, 14, 2, 3)
-    difference = (later_time - first_time).total_seconds()
-    days = divmod(difference, 86400)  # Get days (without [0]!)
-    hours = divmod(days[1], 3600)  # Use remainder of days to calc hours
-    minutes = divmod(hours[1], 60)  # Use remainder of hours to calc minutes
-    seconds = divmod(minutes[1], 1)
-    difference = later_time - first_time
-    context = {
-        'counter': difference,
-    }
-    return render(request, 'countdown/index.html', context)
+    return render(request, 'countdown/counter.html')
